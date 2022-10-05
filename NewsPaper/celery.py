1b3b7dbfde759c777fe72fcf238e2d841fd7ac6e
Victor Kornienko <raidhorr@gmail.com>
2022-10-05 12:52:12 +0300

    задачи по расписанию

--- NewsPaper/celery.py
+++ NewsPaper/celery.py
@@ -1,5 +1,7 @@
 import os
+import datetime
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
 
@@ -7,3 +9,12 @@ app = Celery('NewsPaper')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'action_every_monday_8am': {
+        'task': 'news.tasks.review',
+        'schedule': 5,
+    },
+}
+
+
--- news/tasks.py
+++ news/tasks.py
@@ -1,17 +1,31 @@
 from celery import shared_task
 from .models import Post, User
 from django.core.mail import send_mail
+from datetime import datetime, timedelta
 
 
 @shared_task
 def email_article(pk):
     author = Post.objects.get(pk=pk).author
-    # mail_list = [rec['email'] for rec in User.objects.all.values('email')]
-    # print(mail_list)
+    mail_list = [usr.email for usr in User.objects.all() if usr.email]
 
     send_mail(
         subject=f'{author} разместил новое сообщение',
         message=f'{Post.objects.get(pk=pk).text}',
         from_email='raidhorr@gmail.com',
-        recipient_list=['tehotdel@itcprof.com']
+        recipient_list=mail_list
+    )
+
+
+@shared_task
+def review():
+    mail_list = [usr.email for usr in User.objects.all() if usr.email]
+    message = ''
+    for post in Post.object.filter(in_time_gt=datetime.now()-timedelta(days=7)):
+        message += f'{post.preview()}\n'
+    send_mail(
+        subject=f'Обзор сообщений за  неделю',
+        message=message,
+        from_email='raidhorr@gmail.com',
+        recipient_list=mail_list
     )
